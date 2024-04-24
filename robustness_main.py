ef6daaca3d0c799aa34ad607d601906dbb266280
snownus <snownus@gmail.com>
2024-04-24 07:42:42 +0000

    clean codes

--- robustness_main.py
+++ robustness_main.py
@@ -8,14 +8,14 @@ import torch
 import pandas as pd
 
 def save_losses(dataset, sgd_train_losses, adam_train_losses, SDP_train_losses, 
-                sgd_test_losses, adam_test_losses, SDP_test_losses):
+                sgd_test_losses, adam_test_losses, SDP_test_losses, lr):
     data = {
         "SGD": sgd_train_losses,
         "Adam": adam_train_losses, 
         "Ours": SDP_train_losses
     }
     df = pd.DataFrame(data)
-    df.to_csv(dataset + '_train1.csv', index=False)
+    df.to_csv(dataset + '_' + str(lr) + '_' + 'train.csv', index=False)
 
     data = {
         "SGD": sgd_test_losses,
@@ -23,31 +23,34 @@ def save_losses(dataset, sgd_train_losses, adam_train_losses, SDP_train_losses,
         "Ours": SDP_test_losses
     }
     df = pd.DataFrame(data)
-    df.to_csv(dataset + '_test1.csv', index=False)
+    df.to_csv(dataset  + '_' + str(lr) + '_' +  'test.csv', index=False)
 
 
 if __name__ == '__main__':
     hidden_channels = 500
+    lr = 0.001
     K, L, scale = 4, 40, 100
-    complete_data, random_data = generate_complete_graph_data(), generate_random_graph_data
+    complete_data, random_data = generate_complete_graph_data(), generate_random_graph_data()
 
-    for dataset, dataloader in zip(['complete, random'], [complete_data, random_data]):
+    for dataset, dataloader in zip(['complete', 'random'], [complete_data, random_data]):
         train_data, test_data, input_channels = dataloader
         
         BASE1 = models(dataset, "BASE", hidden_channels, -1, -1, input_channels=input_channels)
-        sgd_optimizer = torch.optim.SGD(BASE1.parameters(), lr=0.01)
+        sgd_optimizer = torch.optim.SGD(BASE1.parameters(), lr=lr)
         sgd_train_losses, sgd_test_losses = train(BASE1, sgd_optimizer, train_data, test_data)
 
         BASE2 = models(dataset, "BASE", hidden_channels, -1, -1, input_channels=input_channels)
-        adam_optimizer = torch.optim.SGD(BASE1.parameters(), lr=0.01)
+        adam_optimizer = torch.optim.SGD(BASE1.parameters(), lr=lr)
         adam_train_losses, adam_test_losses = train(BASE1, sgd_optimizer, train_data, test_data)
 
         SDP_model = models(dataset, "SDP", hidden_channels, K, scale, input_channels=input_channels)
-        sdp_optimizer = torch.optim.SGD(SDP_model.parameters(), lr=0.01)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        SDP_model.to(device)
+        sdp_optimizer = torch.optim.SGD(SDP_model.parameters(), lr=lr)
         SDP_train_losses, SDP_test_losses = train_sdp(SDP_model, sdp_optimizer, train_data, test_data)
 
         save_losses(dataset, sgd_train_losses, adam_train_losses, SDP_train_losses, 
-                    sgd_test_losses, adam_test_losses, SDP_test_losses)
+                    sgd_test_losses, adam_test_losses, SDP_test_losses, lr)
 
         i = 0
         for inputs, targets in test_data:
@@ -73,4 +76,4 @@ if __name__ == '__main__':
             "pertubation": lams
         }
         df = pd.DataFrame(data)
-        df.to_csv(dataset + '_hessian1.csv', index=False)
+        df.to_csv(dataset  + '_' + str(lr) + '_' +  'hessian.csv', index=False)
