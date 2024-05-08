d9feca17c34986b1a35e70116cef79d9d9842333
snownus <snownus@gmail.com>
2024-05-08 13:21:25 +0000

    resnet for 1W32A

--- models/sdp_resnet.py
+++ models/sdp_resnet.py
@@ -13,7 +13,7 @@ import torch.nn.init as init
 from .sdp_wo_entropy import BinarizeConv2dSDP
 
 
-__all__ =['resnet18_1w1a']
+__all__ =['resnet18_1w1a', 'resnet18_1w32a']
 
 def _weights_init(m):
     if isinstance(m, nn.Conv2d):
@@ -26,6 +26,34 @@ class LambdaLayer(nn.Module):
 
     def forward(self, x):
         return self.lambd(x)
+
+
+class BasicBlock_1w32a(nn.Module):
+    expansion = 1
+
+    def __init__(self, K, scale, in_planes, planes, stride=1):
+        super(BasicBlock_1w32a, self).__init__()
+        self.conv1 = BinarizeConv2dSDP(K, scale, in_planes, planes, kernel_size=3, stride=stride, padding=1, binarize_a=False, binarize_out=False, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = BinarizeConv2dSDP(K, scale, planes, planes, kernel_size=3, stride=1, padding=1, binarize_a=False, binarize_out=False, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        pad = 0 if planes == self.expansion*in_planes else planes // 4
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                        nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
+                        nn.BatchNorm2d(self.expansion*planes, affine=False, eps=1e-5, momentum=0.2)
+                        )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)), inplace=True)
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out, inplace=True)
+        return out
+
+
 
 class BasicBlock_1w1a(nn.Module):
     expansion = 1
@@ -82,9 +110,10 @@ class Bottleneck_1w1a(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, K, scale, block, num_blocks, num_channel, num_classes=10):
+    def __init__(self, K, scale, block, num_blocks, num_channel, binarize_a=True, num_classes=10):
         super(ResNet, self).__init__()
         self.in_planes = num_channel[0]
+        self.binarize_a = binarize_a
 
         self.conv1 = nn.Conv2d(3, num_channel[0], kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(num_channel[0])
@@ -106,7 +135,10 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = self.bn1(self.conv1(x))
+        if self.binarize_a:
+            out = self.bn1(self.conv1(x))
+        else:
+            out = F.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
@@ -128,6 +160,19 @@ def resnet18_1w1a(**kwargs):
     elif dataset == 'cifar100':
         num_classes = 100
     return ResNet(K, scale, BasicBlock_1w1a, [2,2,2,2], [64,128,256,512], num_classes=num_classes)
+
+
+def resnet18_1w32a(**kwargs):
+    K = kwargs.get( 'K', 2)
+    scale = kwargs.get('scale', 100)
+    dataset = kwargs.get('dataset', 'cifar10')
+    num_classes = -1
+    if dataset == 'cifar10':
+        num_classes = 10
+    elif dataset == 'cifar100':
+        num_classes = 100
+    return ResNet(K, scale, BasicBlock_1w32a, [2,2,2,2], [64,128,256,512], num_classes=num_classes)
+
 
 def resnet34_1w1a(**kwargs):
     return ResNet(BasicBlock_1w1a, [3,4,6,3],[64,128,256,512],**kwargs)
