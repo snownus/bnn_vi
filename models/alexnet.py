a41e17423460cf9d837d0813d2ddd78e311a8651
snownus <snownus@gmail.com>
2024-05-12 12:15:20 +0000

    alexnet and birealnet 1w32a with relu

--- models/alexnet.py
+++ models/alexnet.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 
 from .sdp_wo_entropy import BinarizeConv2dSDP
 
-__all__ = ['AlexNet', 'alexnet_1w1a', 'alexnet_1w32a']
+__all__ = ['AlexNet', 'AlexNet_1W32A', 'alexnet_1w1a', 'alexnet_1w32a']
 
 class AlexNet(nn.Module):
 
@@ -44,6 +44,50 @@ class AlexNet(nn.Module):
         return x
 
 
+class AlexNet_1W32A(nn.Module):
+
+    def __init__(self, K, scale, num_classes=1000):
+        super(AlexNet_1W32A, self).__init__()
+        self.num_classes = num_classes
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=2),
+            nn.BatchNorm2d(96),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            BinarizeConv2dSDP(K, scale, 96, 256, kernel_size=5, stride=1, padding=2, bias=True, binarize_a=False),
+            nn.BatchNorm2d(256, eps=1e-3, momentum=0.1, affine=True),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            BinarizeConv2dSDP(K, scale, 256, 384, kernel_size=3, stride=1, padding=1, bias=True, binarize_a=False),
+            nn.BatchNorm2d(384, eps=1e-3, momentum=0.1, affine=True),
+            nn.ReLU(inplace=True),
+            BinarizeConv2dSDP(K, scale, 384, 384, kernel_size=3, stride=1, padding=1, bias=True, binarize_a=False),
+            nn.BatchNorm2d(384, eps=1e-3, momentum=0.1, affine=True),
+            nn.ReLU(inplace=True),
+            BinarizeConv2dSDP(K, scale, 384, 256, kernel_size=3, stride=1, padding=1, bias=True, binarize_a=False),
+            nn.BatchNorm2d(256, eps=1e-3, momentum=0.1, affine=True),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            BinarizeConv2dSDP(K, scale, 256 * 6 * 6, 4096, kernel_size=1, padding=0, linear=True, bias=True, binarize_a=False),
+            nn.BatchNorm1d(4096, eps=1e-3, momentum=0.1, affine=True),
+            nn.ReLU(inplace=True),
+            BinarizeConv2dSDP(K, scale, 4096, 4096, kernel_size=1, padding=0, linear=True, bias=True, binarize_a=False),
+            nn.BatchNorm1d(4096, eps=1e-3, momentum=0.1, affine=True),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 256 * 6 * 6)
+        x = self.classifier(x)
+        return x
+
+
+
 def alexnet_1w1a(pretrained=False, **kwargs):
     r"""AlexNet model architecture from the
     `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
@@ -66,10 +110,10 @@ def alexnet_1w32a(pretrained=False, **kwargs):
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
-    model = AlexNet(**kwargs, binarize_a=False)
+    model = AlexNet_1W32A(**kwargs)
     if pretrained:
         model_path = 'model_list/alexnet.pth.tar'
         pretrained_model = torch.load(model_path)
         model.load_state_dict(pretrained_model['state_dict'])
-        
+
     return model
--- models/birealnet_recu.py
+++ models/birealnet_recu.py
@@ -11,26 +11,31 @@ BN = None
 __all__ = ['resnet18_1w1a_recu', 'resnet34_1w1a_recu']
 
 
-def conv3x3Binary(K, scale, binary_opt, in_planes, out_planes, stride=1):
+def conv3x3Binary(K, scale, binarize_a, in_planes, out_planes, stride=1):
     "3x3 convolution with padding"
     # return BinarizeConv2d(in_planes, out_planes, kernel_size=3, stride=stride,
     #                 padding=1, bias=False)
     return BinarizeConv2dSDP(K, scale, in_planes, out_planes, kernel_size=3, 
-                             stride=stride, padding=1, binarize_a=False, 
-                             binarize_opt=binary_opt)
+                             stride=stride, padding=1, binarize_a=binarize_a)
 
 
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, K, scale, binary_opt, inplanes, planes, stride=1, downsample=None):
+    def __init__(self, K, scale, binarize_a, inplanes, planes, stride=1, downsample=None):
         super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3Binary(K, scale, binary_opt, inplanes, planes, stride)
+        self.conv1 = conv3x3Binary(K, scale, binarize_a, inplanes, planes, stride)
         self.bn1 = BN(planes)
-        self.nonlinear1 = nn.Hardtanh(inplace=True)
-        self.conv2 = conv3x3Binary(K, scale, binary_opt, planes, planes)
+        if binarize_a:
+            self.nonlinear1 = nn.Hardtanh(inplace=True)
+        else:
+            self.nonlinear1 = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3Binary(K, scale, binarize_a, planes, planes)
         self.bn2 = BN(planes)
-        self.nonlinear2 = nn.Hardtanh(inplace=True)
+        if binarize_a:
+            self.nonlinear2 = nn.Hardtanh(inplace=True)
+        else:
+            self.nonlinear2 = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
 
@@ -57,7 +62,7 @@ class BasicBlock(nn.Module):
 
 class ResNet(nn.Module):
 
-    def __init__(self, block, layers, K, scale, binary_opt, num_classes=1000, 
+    def __init__(self, block, layers, K, scale, binarize_a, num_classes=1000, 
                  avg_down=False, bypass_last_bn=False,
                  bn_group_size=1,
                  bn_group=None,
@@ -77,12 +82,15 @@ class ResNet(nn.Module):
         
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = BN(64)
-        self.nonlinear = nn.Hardtanh(inplace=True)
+        if binarize_a:
+            self.nonlinear = nn.Hardtanh(inplace=True)
+        else:
+            self.nonlinear = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(K, scale, binary_opt, block, 64, layers[0])
-        self.layer2 = self._make_layer(K, scale, binary_opt, block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(K, scale, binary_opt, block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(K, scale, binary_opt, block, 512, layers[3], stride=2)
+        self.layer1 = self._make_layer(K, scale, binarize_a, block, 64, layers[0])
+        self.layer2 = self._make_layer(K, scale, binarize_a, block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(K, scale, binarize_a, block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(K, scale, binarize_a, block, 512, layers[3], stride=2)
         self.avgpool = nn.AvgPool2d(7, stride=1)
         self.bn2 = nn.BatchNorm1d(512 * block.expansion)
         self.fc = nn.Linear(512 * block.expansion, num_classes)
@@ -99,7 +107,7 @@ class ResNet(nn.Module):
                 param.data.zero_()
             print('bypass {} bn.weight in BottleneckBlocks'.format(len(bypass_bn_weight_list)))
 
-    def _make_layer(self, K, scale, binary_opt, block, planes, blocks, stride=1, avg_down=False):
+    def _make_layer(self, K, scale, binarize_a, block, planes, blocks, stride=1, avg_down=False):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             if self.avg_down:
@@ -117,10 +125,10 @@ class ResNet(nn.Module):
                 )
 
         layers = []
-        layers.append(block(K, scale, binary_opt, self.inplanes, planes, stride, downsample))
+        layers.append(block(K, scale, binarize_a, self.inplanes, planes, stride, downsample))
         self.inplanes = planes * block.expansion
         for i in range(1, blocks):
-            layers.append(block(K, scale, binary_opt, self.inplanes, planes))
+            layers.append(block(K, scale, binarize_a, self.inplanes, planes))
 
         return nn.Sequential(*layers)
 
@@ -146,6 +154,12 @@ class ResNet(nn.Module):
 def resnet18_1w1a_recu(**kwargs):
     """Constructs a ResNet-18 model. """
     model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
+    return model
+
+
+def resnet18_1w32a_recu(**kwargs):
+    """Constructs a ResNet-18 model. """
+    model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs, binarize_a=False)
     return model
 
 
